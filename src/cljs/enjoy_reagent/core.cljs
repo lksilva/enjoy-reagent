19c46358b47f14cf3a3341f140dfed6ab0536a0d
Lucas Souza <lucas_s.s@hotmail.com>
2023-08-26 15:29:25 -0300

    Moving visually blue box

--- src/cljs/enjoy_reagent/core.cljs
+++ src/cljs/enjoy_reagent/core.cljs
@@ -4,11 +4,23 @@
    [reagent.dom :as rdom]
    [clerk.core :as clerk]))
 
+;; TODO:
+;; Move the blue box
+;; Be able to drag and drop blue box
+;; Show visible area of red box
+;; Identify when a blue is on top of red
+;; Calculate the difference when a blue box is on top of red box
 (defn red-box []
   [:div.red-box])
 
 (defn blue-box []
-  [:div.blue-box])
+  (let [xy (reagent/atom {:x 0 :y 0})]
+    (fn []
+      [:div.blue-box
+       {:style {:left (str (:x @xy) "px")
+                :top (str (:y @xy) "px")}
+        :draggable true}
+       "Move"])))
 
 (defn current-page []
   (fn []
