6666d7341b67db1d62a0c5e16876f9e67bd22a95
Lucas Souza <lucas_s.s@hotmail.com>
2023-08-26 18:35:40 -0300

    Being able to calc the area of red box

--- src/cljs/enjoy_reagent/core.cljs
+++ src/cljs/enjoy_reagent/core.cljs
@@ -1,15 +1,23 @@
 (ns enjoy-reagent.core
   (:require
    [reagent.dom :as rdom]
+   [reagent.core :as r]
    [clerk.core :as clerk]))
 
+(def initial-state
+  {:width 200
+   :height 250})
+
 ;; TODO:
-;; Show visible area of red box
 ;; Identify when a blue is on top of red
+;; Do the calc of red container dynamic discading the initial state value
 ;; Calculate the difference when a blue box is on top of red box
-(defn red-box []
+;; Apply space between the moved blue-box 
+(defn red-box [initial-state visible-area]
   [:div.red-box
-   {:on-drag-over
+   {:style {:width (:width initial-state)
+            :height (:height initial-state)}
+    :on-drag-over
     (fn [e]
       (.preventDefault e))
     :on-drop
@@ -18,6 +26,12 @@
             element (-> js/document (.getElementById id))
             available-zone (-> e .-target)]
         (.appendChild available-zone element)
+        (.log js/console e)
+        (.log js/console (-> e .-target .getBoundingClientRect))
+        (.log js/console (-> element .getBoundingClientRect))
+        (swap! visible-area assoc
+               :x (- (-> available-zone .getBoundingClientRect .-width) (-> element .getBoundingClientRect .-width))
+               :y (- (-> available-zone .getBoundingClientRect .-height) (-> element  .getBoundingClientRect .-height)))
         ))}
    ""])
 
@@ -33,20 +47,24 @@
             (.setData "text/plain" (-> e .-target .-id)))
           ;; TODO: in the drag over movement, set origina color
         (set! (.. e -currentTarget -style -backgroundColor) "#c8dfff")
-        (set! (.. e -currentTarget -style -marginBottom) "10px"))}
+        #_(set! (.. e -currentTarget -style -marginBottom) "5px"))}
      ""]))
 
 (defn current-page []
-  (fn []
-    [:div.container
-     [:div.left
-      [blue-box {:id "bb1"}]
-      [blue-box {:id "bb2"}]
-      [blue-box {:id "bb3"}]
-      [blue-box {:id "bb4"}]
-      [blue-box {:id "bb5"}]]
-     [:div.right
-      [red-box]]]))
+  (let [visible-area (r/atom {:x (:width initial-state) :y (:height initial-state)})]
+    (fn []
+      [:<>
+       [:div.container
+        [:div.left
+         [blue-box {:id "bb1"}]
+         [blue-box {:id "bb2"}]
+         [blue-box {:id "bb3"}]
+         [blue-box {:id "bb4"}]
+         [blue-box {:id "bb5"}]]
+        [:div.right
+         [red-box initial-state visible-area]]]
+       [:div.description
+        [:span (str "Total visible area: " (* (:x @visible-area) (:y @visible-area)) "px")]]])))
 
 ;; -------------------------
 ;; Initialize app
